5e60c215fed620f135cdba1565657d66beb3e34a
Wang Zuo <wzuoadjusted@gmail.com>
2017-07-15 10:46:39 +0800

    update test.js

--- test.js
+++ test.js
@@ -1,27 +1,25 @@
 const Arel = require('arel');
 const { Table } = Arel;
 
-describe('test build', () => {
-  test('toSql', () => {
-    const users = new Table('users');
-    const photos = new Table('photos');
-    expect(users.project(Arel.sql('*')).toSql()).toBe(`SELECT * FROM "users"`);
+test('toSql', () => {
+  const users = new Table('users');
+  const photos = new Table('photos');
+  expect(users.project(Arel.sql('*')).toSql()).toBe(`SELECT * FROM "users"`);
 
-    expect(users.project(users.column('id')).toSql()).toBe(
-      `SELECT "users"."id" FROM "users"`
-    );
+  expect(users.project(users.column('id')).toSql()).toBe(
+    `SELECT "users"."id" FROM "users"`
+  );
 
-    expect(users.where(users.column('name').eq('amy')).toSql()).toBe(
-      `SELECT FROM "users" WHERE "users"."name" = 'amy'`
-    );
+  expect(users.where(users.column('name').eq('amy')).toSql()).toBe(
+    `SELECT FROM "users" WHERE "users"."name" = 'amy'`
+  );
 
-    expect(
-      users
-        .join(photos)
-        .on(users.column('id').eq(photos.column('user_id')))
-        .toSql()
-    ).toBe(
-      `SELECT FROM "users" INNER JOIN "photos" ON "users"."id" = "photos"."user_id"`
-    );
-  });
+  expect(
+    users
+      .join(photos)
+      .on(users.column('id').eq(photos.column('user_id')))
+      .toSql()
+  ).toBe(
+    `SELECT FROM "users" INNER JOIN "photos" ON "users"."id" = "photos"."user_id"`
+  );
 });
